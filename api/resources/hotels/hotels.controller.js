d6135d1f8e3b55ed293d0e2e6fc6678c923e84cd
uwu <afbritezr41@alumnes.santjosepobrer.es>
2021-02-16 20:14:35 +0100

    Paralel error handling, average, top3

--- api/resources/hotels/hotels.controller.js
+++ api/resources/hotels/hotels.controller.js
@@ -1,19 +1,57 @@
 const axios = require("axios")
 
+const PROTOCOL = process.env.PROTOCOL
 const HOST = process.env.HOST
 const ROUTE = process.env.ROUTE
 const SHA1 = process.env.SHA1
+const APIHEADER  = {headers: {"x-api-key": SHA1}}
 
-const APIHEADER  = {
-    headers: {
-      "x-api-key": SHA1,
-    }
-  }
+const COUNTRIES = process.env.COUNTRIES.split(",")
+var COUNTRYURLS = COUNTRIES.map((element) => PROTOCOL + HOST + ROUTE.replace("*", element) ) 
+var RESULT = []
+
 getHotels = async () => {
-    return axios.get(`https://${HOST}${ROUTE}`, APIHEADER)
+    if (handleErrorsAndSuccess(await Promise.all(COUNTRYURLS.map((url) => axios.get(url, APIHEADER)).map(p => p.catch(e => e))))){
+        return getHotels()
+    } else {
+        return returnToNormal()
+    } 
 }
-
-
+handleErrorsAndSuccess = (RESULTS) => {
+    var FAILED = []
+    RESULTS.forEach((result, i)=> {
+        if (!(result instanceof Error)){
+            COUNTRYURLS.splice(i,1)
+            RESULT.push(result.data)
+        } else if (result instanceof Error) {
+            FAILED.push(i)
+        }
+    })
+    return FAILED.length > 0
+}
+returnToNormal = () => {
+    const finalresult = RESULT
+    COUNTRYURLS = COUNTRIES.map((element) => PROTOCOL + HOST + ROUTE.replace("*", element)) 
+    RESULT = []
+    return finalresult
+}
+getAverage = (hotels) => {
+    return hotels.map(element => element.reduce((r, c) => r + c.score, 0) / element.length) 
+}
+getTop3 = (hotels) => {
+    return hotels.map(element => element.sort((a, b) => {return  b.score - a.score}).slice(0,3))
+}
+responseBuilder = (average, top3) => {
+    var response = COUNTRIES.reduce(function(obj, v) {
+        obj[v] = 0;
+        return obj;
+      }, {})
+    console.log(response)
+    return response
+}
 module.exports = {
-    getHotels
+    getHotels,
+    getAverage,
+    getTop3,
+    responseBuilder
 }
--- api/resources/hotels/hotels.routes.js
+++ api/resources/hotels/hotels.routes.js
@@ -6,14 +6,13 @@ const processErrors = require('../../libs/errorHandler').processAPIError;
 hotelsRouter.get("/", [processErrors], async (req,res) => {
     try{
         const hotels = await hotelsController.getHotels()
-        res.send(hotels.data)
-    }catch(err){
-        if (err.response.status === 503){
-            console.log("Aplication failed randomly, trying again")
-            hotelsRouter.handle(req, res)
-        }else{
-            res.send(err)
-        }
+        const average = hotelsController.getAverage(hotels)
+        const top3 = hotelsController.getTop3(hotels)
+        const response = hotelsController.responseBuilder(average, top3)
+        res.send(top3)
+    } catch(err) {
+        console.log(err)
+        res.send(err)
     }
 })
 
